e745404d4f32aead256ceba3edd708f7380b4a7e
TavLiipe <filipetavares0990@gmail.com>
2025-10-25 13:31:13 -0300

    Updated OrderList.tsx

--- src/components/admin/OrderList.tsx
+++ src/components/admin/OrderList.tsx
@@ -265,7 +265,7 @@ const OrderList: React.FC<OrderListProps> = ({ orders, onSelectOrder, onEditOrde
                     </td>
                     <td className="px-6 py-6 whitespace-nowrap text-sm text-gray-900 dark:text-gray-100">
   {format(new Date(order.delivery_date), 'dd/MM/yyyy HH:mm', { locale: ptBR })}
-                  </td>    
+                    </td> 
                     <td className="px-6 py-6 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">
                       {format(new Date(order.created_at), 'dd/MM/yyyy', { locale: ptBR })}
                     </td>
