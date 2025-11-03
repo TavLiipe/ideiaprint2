fba694d664eaf978159da69bbb17948a20b511d5
TavLiipe <filipetavares0990@gmail.com>
2025-11-03 13:16:38 -0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,6 +7,9 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black/60"></div>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
         <div className="grid lg:grid-cols-2 gap-12 items-center">
+          {/* Content */}
+          <div className="space-y-8">
+            <div className="space-y-4">
             </div>
 
             {/* Features */}
