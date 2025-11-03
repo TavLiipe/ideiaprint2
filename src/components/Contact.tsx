7b8df8daf0ba1786e01b0dfa98d6ff97cc73fa7c
TavLiipe <filipetavares0990@gmail.com>
2025-11-03 13:01:13 -0300

    Updated Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -104,17 +104,18 @@ const Contact = () => {
 
             {/* Map */}
             {/* Map */}
-            <div className="rounded-xl overflow-hidden h-64 border border-gray-800">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.834160255074!2d-46.65657198447691!3d-23.57721796849852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59c8e25e89f1%3A0x9c315a2bfb8bbdc3!2sAv.%20Paulista%2C%201000%20-%20Bela%20Vista%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001310-100!5e0!3m2!1spt-BR!2sbr!4v1699020412301!5m2!1spt-BR!2sbr"
-                width="100%"
-                height="100%"
-                style={{ border: 0 }}
-                allowFullScreen=""
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-              ></iframe>
-            </div>
+<div className="rounded-xl overflow-hidden h-64 border border-gray-800">
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.834160255074!2d-46.65657198447691!3d-23.57721796849852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59c8e25e89f1%3A0x9c315a2bfb8bbdc3!2sAv.%20Paulista%2C%201000%20-%20Bela%20Vista%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001310-100!5e0!3m2!1spt-BR!2sbr!4v1699020412301!5m2!1spt-BR!2sbr"
+    width="100%"
+    height="100%"
+    style={{ border: 0 }}
+    allowFullScreen=""
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+  ></iframe>
+</div>
+          </div>
 
           {/* Contact Form */}
           <div className="bg-white rounded-2xl p-8 shadow-lg">
